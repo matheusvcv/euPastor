85a7edff15830cebadce79cb085b8fbe074745e5
Matheus Vinicius <matheusvcv@yahoo.com.br>
2023-06-14 19:53:23 -0300

    Criação da estrutura de pastas do projeto. Dessenvolvimento do arquivo index e adicionado imagem de fundo.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width initial-scale=1.0">
+	<!--<link rel="stylesheet" type="text/css" href="src/style.css">-->
+	<style>
+		body{
+			background-image: url(img/bg_image.jpg);
+			background-position: left center;
+			background-attachment: fixed;
+		}
+	</style>
+	<link rel="shortcut icon" href="img/icon_image.png" type="image/x-icon">
+	<title>Login</title>
+</head>
+<body>
+
+</body>
+</html>
