6e0e7385df17c76769799c6ffa0189a97ee03770
Matheus Vinicius <matheusvcv@yahoo.com.br>
2023-06-16 09:59:41 -0300

    Desenvolvido container com formulário de login

--- index.php
+++ index.php
@@ -9,7 +9,44 @@
 	<title>Login</title>
 </head>
 <body>
-
+	<div class="container-fluid p-5 mt-4">
+		<div class="row">
+			<div class="col-lg-5 col-sm-12 col-md-8">
+				<div class="container border rounded p-5 mt-4 bg_login">
+					<div class="text-center">
+						<img  class=""  src="img/logo_login_image.png" alt="Logo Bíblia">
+					</div>
+					<form>
+						<div class="row">
+							<div class="col-12 mb-1">
+						    	<label for="nome_usuario" class="form-label">Nome de Usuário:</label>
+						    	<input type="text" class="form-control" id="nome_usuario">
+							</div>
+						</div>
+						 <div class="row">
+						 	 <div class="col-12 mb-1">
+								<label for="senha" class="form-label">Senha:</label>
+								<input type="password" class="form-control" id="senha">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-6 mb-2">
+								<a href="auto_cadastro.php" class="card-link">Cadastre-se</a>	
+							</div>
+							 <div class="col-6 mb-2 float-sm-left text-center text-sm-right">
+								<a href="auto_cadastro.php" class="card-link">Esqueci minha senha</a>	
+							</div>
+						</div>
+						<div class="row">
+						 	<div class="6">
+						 		<a type="submit" class="btn btn-primary col-12">Entrar</a>
+						 	</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
