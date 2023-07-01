85e0046c264b6ba10f66f4477fa9e114a517373d
Matheus Vinicius <matheusvcv@yahoo.com.br>
2023-06-30 21:06:41 -0300

    Adicionado jquery no arquivo configuracoes_membros.php. Desenvolvida função que exiibe o o formulário quando abertat no botão 'cadastrar membro'. Desenvolvido botão 'Cadastrar Membro.

--- configuracao_membros.php
+++ configuracao_membros.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	<link rel="shortcut icon" href="img/logo_login_image.png" type="image/x-icon">
+	<script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
 	<title>Cadastro de Membros</title>
 </head>
 <body>
@@ -56,98 +57,100 @@
 	<div class="card card_body m-3">
 		<div class="card-content collapse show">
 			<div class="card-body card-dashboard">
-				<div id="cadastro_usuario">
-					
-				
-				<!--Início do formulário-->
-				<h3>Cadastro de Membros</h3>
-				<div class="container-fluid">
-					<form class="mt-3" method="POST" action="../ws/ws_cadastrar_usuarios.php">
-						<div class="row mt-3">
-							<h6>***</h6>
-						</div>
-						<div class="row">
-							<div class="col-lg-4">
-								<label for="nome" class="form-label">Nome do membro:</label>
-								<input type="text" class="form-control form_item" id="nome_membro" name="nome_membro">
+				<div id="cadastro_usuario" name="cadastro_usuario">
+					<!--Início do formulário-->
+					<h3>Cadastro de Membros</h3>
+					<div class="container-fluid">
+						<form class="mt-3" method="POST" action="../ws/ws_cadastrar_usuarios.php">
+							<div class="row mt-3">
+								<h6>***</h6>
 							</div>
-							<div class="col-lg-4">
-								<label for="telefone" class="form-label">CPF do mebro:</label>
-								<input type="text" class="form-control form_item" id="cpf_membro" name="cpf_membro">
+							<div class="row">
+								<div class="col-lg-4">
+									<label for="nome" class="form-label">Nome do membro:</label>
+									<input type="text" class="form-control form_item" id="nome_membro" name="nome_membro">
+								</div>
+								<div class="col-lg-4">
+									<label for="telefone" class="form-label">CPF do mebro:</label>
+									<input type="text" class="form-control form_item" id="cpf_membro" name="cpf_membro">
+								</div>
+								<div class="col-lg-4">
+									<label for="nascimento" class="form-label">Data de Nascimento:</label>
+									<input type="date" class="form-control form_item" id="nascimento_membro" name="nascimento_membro">
+								</div>
 							</div>
-							<div class="col-lg-4">
-								<label for="nascimento" class="form-label">Data de Nascimento:</label>
-								<input type="date" class="form-control form_item" id="nascimento_membro" name="nascimento_membro">
+							<div class="row mt-3">
+								<h6>******</h6>
 							</div>
-						</div>
-						<div class="row mt-3">
-							<h6>******</h6>
-						</div>
-						<div class="row">
-							<div class="col-lg-4">
-								<label for="email" class="form-label">Endereço de e-mail:</label>
-								<input type="email" class="form-control form_item" id="email_membro" name="email_membro">
+							<div class="row">
+								<div class="col-lg-4">
+									<label for="email" class="form-label">Endereço de e-mail:</label>
+									<input type="email" class="form-control form_item" id="email_membro" name="email_membro">
+								</div>
+								<div class="col-lg-4">
+									<label for="nome_usuario" class="form-label">Telefone:</label>
+									<input type="text" class="form-control form_item" id="telefone_membro" name="telefone_membro">
+								</div>
+								<div class="col-lg-4">
+									<label for="cargo" class="form-label">Quanto tempo este membro faz parte da igreja?</label>
+									<select class="form-select form_item" id="tempo" name="tempo">
+										<option value="1">Membro Novo</option>
+										<option value="2">De 01 a 03 anos</option>
+										<option value="3">De 03 a 05 anos</option>
+										<option value="4">Mais de 05 anos</option>
+									</select>
+								</div>
 							</div>
-							<div class="col-lg-4">
-								<label for="nome_usuario" class="form-label">Telefone:</label>
-								<input type="text" class="form-control form_item" id="telefone_membro" name="telefone_membro">
+							<div class="row mt-3">
+								<h6>******</h6>
 							</div>
-							<div class="col-lg-4">
-								<label for="cargo" class="form-label">Quanto tempo este membro faz parte da igreja?</label>
-								<select class="form-select form_item" id="tempo" name="tempo">
-									<option value="1">Membro Novo</option>
-									<option value="2">De 01 a 03 anos</option>
-									<option value="3">De 03 a 05 anos</option>
-									<option value="4">Mais de 05 anos</option>
-								</select>
+							<div class="row">
+								<div class="col-lg-4">
+									<label for="cargo" class="form-label">Quanto tempo este membro faz parte da igreja?</label>
+									<select class="form-select form_item" id="tempo" name="tempo">
+										<option value="Membro Novo">Membro Novo</option>
+										<option value="De 01 a 03 anos">De 01 a 03 anos</option>
+										<option value="De 03 a 05 anos">De 03 a 05 anos</option>
+										<option value="Mais de 05 anos">Mais de 05 anos</option>
+									</select>
+								</div>
+								<div class="col-lg-4">
+									<label for="cargo" class="form-label">Este pode ser considerado um membro ativo?</label>
+									<select class="form-select form_item" id="ativo" name="ativo">
+										<option value="Sim">Sim</option>
+										<option value="Nao">Não</option>
+										<option value="Membro novo, ainda não é possível definir.">Membro novo, ainda não é possível definir.</option>
+									</select>
+								</div>
+								<div class="col-lg-4">
+									<label for="nome" class="form-label">Faixa Salarial:</label>
+									<input type="text" class="form-control form_item" id="faixa_salarial" name="faixa_salarial">
+								</div>
 							</div>
-						</div>
-						<div class="row mt-3">
-							<h6>******</h6>
-						</div>
-						<div class="row">
-							<div class="col-lg-4">
-								<label for="cargo" class="form-label">Quanto tempo este membro faz parte da igreja?</label>
-								<select class="form-select form_item" id="tempo" name="tempo">
-									<option value="Membro Novo">Membro Novo</option>
-									<option value="De 01 a 03 anos">De 01 a 03 anos</option>
-									<option value="De 03 a 05 anos">De 03 a 05 anos</option>
-									<option value="Mais de 05 anos">Mais de 05 anos</option>
-								</select>
+							<div class="row justify-content-end mb-2">
+								<div class="col-lg-1 col-sm-12 mt-3">
+									<button type="submit" class="btn btn-success form-control form_item">Cadastrar</button>
+								</div>
+								<div class="col-lg-1 mt-3">
+									<a href="../../pagina_inicial.php?usuario_cadastrado=2"><div class="btn btn-danger form-control form_item">Cancelar</div></a>
+								</div>
 							</div>
-							<div class="col-lg-4">
-								<label for="cargo" class="form-label">Este pode ser considerado um membro ativo?</label>
-								<select class="form-select form_item" id="ativo" name="ativo">
-									<option value="Sim">Sim</option>
-									<option value="Nao">Não</option>
-									<option value="Membro novo, ainda não é possível definir.">Membro novo, ainda não é possível definir.</option>
-								</select>
-							</div>
-							<div class="col-lg-4">
-								<label for="nome" class="form-label">Faixa Salarial:</label>
-								<input type="text" class="form-control form_item" id="faixa_salarial" name="faixa_salarial">
-							</div>
-						</div>
-
-						<div class="row justify-content-end mb-2">
-							<div class="col-lg-1 col-sm-12 mt-3">
-								<button type="submit" class="btn btn-success form-control form_item">Cadastrar</button>
-							</div>
-							<div class="col-lg-1 mt-3">
-								<a href="../../pagina_inicial.php?usuario_cadastrado=2"><div class="btn btn-danger form-control form_item">Cancelar</div></a>
-							</div>
-						</div>
-					</form>
-				</div>	
+						</form>
+					</div>	
+				</div>
+				<button id="exibe_form_cad_membro" class="btn btn-success" onclick="exibe_form_cad_membro();">Cadastrar Membro</button>
 			</div>
-
-
-</div>
-
-
 		</div>
 	</div>
 	<!--Script Bootstrap-->
 	<script src="../../bootstrap/js/bootstrap.min.js"></script>
+	<script>
+
+		function exibe_form_cad_membro()
+		{
+			$('#cadastro_usuario').css('display', 'block');
+		}
+
+	</script>
 </body>
 </html>
