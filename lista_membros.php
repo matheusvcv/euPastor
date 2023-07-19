943ba116435778cf658df39c490fff38785ff55b
Matheus Vinicius <matheusvcv@yahoo.com.br>
2023-07-19 20:23:58 -0300

    Desenvolvida página de exibição de membros. Diferente da 'configuracao_de_membros' essa página será somente para consulta de informações, e não será premitido editar e excluir dados. Todos os níveis de usuáriso terão acesso. Desenvolvida datatable.

--- lista_membros.php
+++ lista_membros.php
@@ -10,7 +10,7 @@
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
 	<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
-	<title>Configuração de Membros</title>
+	<title>Lista de Membros</title>
 </head>
 <body>
 <!--Início da NavBar-->
@@ -60,81 +60,7 @@
 	<div class="card card_body m-3">
 		<div class="card-content collapse show">
 			<div class="card-body card-dashboard">
-				<div id="cadastro_usuario" name="cadastro_usuario">
-					<!--Início do formulário-->
-					<h3>Cadastro de Membros</h3>
-					<div class="container-fluid">
-						<form  method="POST" name="form_inserir_membros" id="form_inserir_membros">
-							<div class="row mt-3">
-								<h6>Dados Pessoais</h6>
-							</div>
-							<div class="row mb-4">
-								<div class="col-lg-4">
-									<label for="nome" class="form-label">Nome do membro:</label>
-									<input type="text" class="form-control form_item" id="nome_membro" name="nome_membro" placeholder="Digite o nome aqui.">
-								</div>
-								<div class="col-lg-4">
-									<label for="telefone" class="form-label">CPF do mebro:</label>
-									<input type="text" class="form-control form_item" id="cpf_membro" name="cpf_membro" placeholder="Digite o CPF aqui.">
-								</div>
-								<div class="col-lg-4">
-									<label for="nascimento" class="form-label">Data de Nascimento:</label>
-									<input type="date" class="form-control form_item" id="nascimento_membro" name="nascimento_membro">
-								</div>
-							</div>
-							<div class="row mt-3">
-								<h6>Dados de contato - Salário</h6>
-							</div>
-							<div class="row mb-4">
-								<div class="col-lg-4">
-									<label for="email" class="form-label">Endereço de e-mail:</label>
-									<input type="email" class="form-control form_item" id="email_membro" name="email_membro" placeholder="Digite o e-mail aqui.">
-								</div>
-								<div class="col-lg-4">
-									<label for="nome_usuario" class="form-label">Telefone Mobile (WhatsApp):</label>
-									<input type="text" class="form-control form_item" id="telefone_membro" name="telefone_membro" placeholder="Digite o telefone aqui.">
-								</div>
-								<div class="col-lg-4">
-									<label for="nome" class="form-label">Faixa Salarial:</label>
-									<input type="text" class="form-control form_item" id="faixa_salarial" name="faixa_salarial" placeholder="Digite o salário aqui.">
-								</div>
-							</div>
-							<div class="row mt-3">
-								<h6>Outras informações</h6>
-							</div>
-							<div class="row mb-4">
-								<div class="col-lg-4">
-									<label for="cargo" class="form-label">Quanto tempo este membro faz parte da igreja?</label>
-									<select class="form-select form_item" id="tempo_de_membro" name="tempo_de_membro">
-										<option value="Membro Novo">Membro Novo</option>
-										<option value="De 01 a 03 anos">De 01 a 03 anos</option>
-										<option value="De 03 a 05 anos">De 03 a 05 anos</option>
-										<option value="Mais de 05 anos">Mais de 05 anos</option>
-									</select>
-								</div>
-								<div class="col-lg-4">
-									<label for="cargo" class="form-label">Este pode ser considerado um membro ativo?</label>
-									<select class="form-select form_item" id="ativo" name="ativo">
-										<option value="Sim">Sim</option>
-										<option value="Nao">Não</option>
-										<option value="Membro novo, ainda não é possível definir.">Membro novo, ainda não é possível definir.</option>
-									</select>
-								</div>
-							</div>
-							<div class="row justify-content-end mb-2">
-								<div class="col-lg-1 col-sm-12 mt-3">
-									<button type="submit" class="btn btn-success form-control form_item" onclick="inserir_membro();">Cadastrar</button>
-								</div>
-								<div class="col-lg-1 mt-3">
-									<div class="btn btn-danger form-control form_item" onclick="esconde_form_cad_membro();">Cancelar</div>
-								</div>
-							</div>
-						</form>
-					</div>	
-				</div>
-				<div>
-					<button id="exibe_form_cad_membro" class="btn btn-success" onclick="exibe_form_cad_membro();">Cadastrar Membro</button>
-				</div>
+
 				<!--Início da DataTable-->
 				<div class="container-fluid">
 					<div class="row mt-3">
@@ -168,44 +94,7 @@
 	<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
 	<script>
 
-	function inserir_membro()
-	{
-		var nome_membro = $('#nome_membro').val();
-		var cpf_membro = $('#cpf_membro').val();
-		var nascimento_membro = $('#nascimento_membro').val();
-		var email_membro = $('#email_membro').val();
-		var telefone_membro = $('#telefone_membro').val();
-		var faixa_salarial = $('#faixa_salarial').val();
-		var tempo_de_membro = $('#tempo_de_membro').val();
-		var ativo = $('#ativo').val();
-
-		$.ajax({
-			url: 'ws/ws_cadastrar_membros.php',
-			type: 'post',
-			data:{
-				'nome_membro': nome_membro,
-				'cpf_membro': cpf_membro,
-				'nascimento_membro': nascimento_membro,
-				'email_membro': email_membro,
-				'telefone_membro': telefone_membro,
-				'faixa_salarial': faixa_salarial,
-				'tempo_de_membro': tempo_de_membro,
-				'ativo': ativo
-			},
-
-		}).done(function(response){
-			
-			if (response === "success")
-			{
-				window.location.href = 'configuracao_membros.php?s=1';
-			}
-
-			if (response === "error")
-			{
-				window.location.href = 'configuracao_membros.php?s=0';
-			}
-		});
-	}
+	
 
 	$(document).ready(function() {
 
@@ -308,23 +197,6 @@
 		}
 	});
 
-
-
-
-
-		
-		function exibe_form_cad_membro()
-		{
-			$('#cadastro_usuario').css('display', 'block');
-			$('#exibe_form_cad_membro').css('display', 'none');
-		}
-
-		
-		function esconde_form_cad_membro()
-		{
-			$('#cadastro_usuario').css('display', 'none');
-			$('#exibe_form_cad_membro').css('display', 'block');
-		}
 
 	</script>
 </body>
