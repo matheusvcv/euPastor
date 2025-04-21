17a7346fc6dafbbafac1c0bf8467788caa5e41a9
matheusvcv <matheusvcv@yahoo.com.br>
2025-04-21 18:40:19 -0300

    Finalizado o backend em PHP do registro de ocorrÊncias. Agora as informações estão sendo inseridas corretamente no BDD.

---
+++ ws/inserir_ocorrencia.php
@@ -0,0 +1,45 @@
+<?php
+	include '../library/libs.php';
+	include '../src/conexao.php';
+
+	if (isset($_POST['id_membro']) && $_POST['id_membro'] != "") {
+
+		$id_membro = $_POST['id_membro'];
+		$titulo = $_POST['titulo'];
+		$ocorrencia = $_POST['ocorrencia'];
+
+		$sql = "
+			INSERT INTO ocorrencias (
+				id_membro,
+				titulo,
+				ocorrencia
+			)
+			VALUES (
+				?,
+				?,
+				?
+			)
+		";
+
+		$statement = $conexao->prepare($sql);
+
+		if (!$statement) {
+			echo "error: " . $conexao->error;
+			exit;
+		}
+
+		$statement->bind_param('iss', $id_membro, $titulo, $ocorrencia);
+
+		if ($statement->execute()) {
+			echo "success";
+		} else {
+			echo "error: " . $statement->error;
+		}
+
+		$statement->close();
+		$conexao->close();
+
+	} else {
+		echo "error: dados não enviados corretamente";
+	}
+?>
