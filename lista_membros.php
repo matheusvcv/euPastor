6df07941c81063d150e86d22193af2b1fe33d1e1
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-15 20:22:59 -0300

    Corrigido erros de identação.

--- lista_membros.php
+++ lista_membros.php
@@ -18,30 +18,30 @@ include "src/protect.php";
 					<button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 				</div>
 				<div class="offcanvas-body">
-						        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active botao_config" aria-current="page" href="logout.php">SAIR</a>
-            <a class="nav-link active botao_config" href="instrucoes.php">Como utilizar o sistema?</a>
-            <li class="nav-item">
-              <a class="nav-link active botao_config" href="" disabled><h7>Módulos:</h7></a>
-            </li>
-		        <li class="nav-item">
-		          <a class="nav-link" href="pagina_inicial.php">Página Inicial</a>
-		        </li>
-            <li class="nav-item">
-              <a class="nav-link" href="lista_membros.php">Lista de Membros</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="escola_biblica_dominical.php">Escola Biblica Dominical</a>
-            </li>
-             <li class="nav-item">
-              <a class="nav-link" href="configuracao_membros.php">Configuração de Membros</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pagina_administrativa/views/cadastrar_usuarios.php">Cadastrar Usuários</a>
-            </li>
-          </li>
-        </ul>
+					<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+		       		   <li class="nav-item">
+		           		 <a class="nav-link active botao_config" aria-current="page" href="logout.php">SAIR</a>
+		            		<a class="nav-link active botao_config" href="instrucoes.php">Como utilizar o sistema?</a>
+		          		  <li class="nav-item">
+		              		<a class="nav-link active botao_config" href="" disabled><h7>Módulos:</h7></a>
+		           		 </li>
+				        <li class="nav-item">
+				          <a class="nav-link" href="pagina_inicial.php">Página Inicial</a>
+				        </li>
+		           		<li class="nav-item">
+		              		<a class="nav-link" href="lista_membros.php">Lista de Membros</a>
+		           		</li>
+		            	<li class="nav-item">
+		              		<a class="nav-link" href="escola_biblica_dominical.php">Escola Biblica Dominical</a>
+		            	</li>
+		            	<li class="nav-item">
+		             		<a class="nav-link" href="configuracao_membros.php">Configuração de Membros</a>
+		           		</li>
+		            	<li class="nav-item">
+		              			<a class="nav-link" href="pagina_administrativa/views/cadastrar_usuarios.php">Cadastrar Usuários</a>
+		            	</li>
+		         	</li>
+       			</ul>
 				</div>
 			</div>
 		</div>
