af4046e5a2000398e206f59a117bc8ad0aff9225
matheusvcv <matheusvcv@yahoo.com.br>
2025-06-24 18:56:44 -0300

    Adicionados comentários no código da página de logout.

--- logout.php
+++ logout.php
@@ -1,9 +1,12 @@
 <?php 
-
+//Verifica se a sessão ainda não foi iniciada.
 if(!isset($_SESSION)){
-		session_start();
-	}
+    //Inicia a sessão.
+    session_start();
+}
 
+//Encerra a sessão do usuário).
 session_destroy();
 
+//Redireciona o usuário para a página de login.
 header('Location: index.php');
