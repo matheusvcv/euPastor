1c32e323621456178738b1b6986978158043d48d
matheusvcv <matheusvcv@yahoo.com.br>
2025-07-20 17:45:52 -0300

    Adicionadas as verificações: ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); no arquivo de conex]ao com o BDD. Agora o sistema vai para mostrar todos os erros, avisos e notificações diretamente na saída, incluindo erros que acontecem durante a inicialização do PHP.

--- src/conexao.php
+++ src/conexao.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 //Essa parte do código estabelece uma conexão com o banco de dados informando servidor, nome de usuário, senha e nome do banco de dados.
 $conexao = new mysqli("localhost", "root", "", "mychurch"); 
 
