1ba84ac44b723726ff5c7e6ccb6a7939df0128b2
matheusvcv <matheusvcv@yahoo.com.br>
2025-07-28 17:32:24 -0300

    Corrigido bugs, e realinhada a tabela da turmar membros.

--- exibir_turmas.php
+++ exibir_turmas.php
@@ -95,24 +95,19 @@ if(!isset($_GET['departamento'])){
 					<div class="row mt-3">
 						<h6>Turmas:</h6>
 					</div>
-					<div class="card" style="width: 18rem;">
-            <table class="table tabela_turmas">
-              <thead>
-                <tr>
-                  <th>Título</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php while($turmas = $resultado_turmas->fetch_assoc()) { ?>
-                  <tr>
-                    <td>
-                      <a class="lista" href="turma.php?id=<?php echo $turmas['id'];  ?>"><?php echo $turmas['nome_turma']; ?></a>
-                    </td>
-                  </tr>
-                  <?php } ?>
-              </tbody>      
-            </table>
-					</div>	
+          <div class="card" style="width: 18rem;">
+              <table class="table tabela_turmas">
+                <tbody>
+                  <?php while($turmas = $resultado_turmas->fetch_assoc()) { ?>
+                    <tr>
+                      <td>
+                        <a class="lista" href="turma.php?id=<?php echo $turmas['id'];  ?>"><?php echo $turmas['nome_turma']; ?></a>
+                      </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>      
+              </table>
+  </div>
 				</div>
 			</div>	
 		</div>
